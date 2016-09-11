272acef43b36175121673a2bfb629add966a3720
rokervich <rokervich@rokervich.com>
2016-09-11 22:20:06 +0200

    initialise array

--- src/main/java/com/openshift/aeg/AccountNumberGenerator.java
+++ src/main/java/com/openshift/aeg/AccountNumberGenerator.java
@@ -1,9 +1,13 @@
 package com.openshift.aeg;
 
 public class AccountNumberGenerator {
+	{
+		accountNumbers= new String[]{"werwe", "fsdfsf"};
+	}
 	
-	public String[] generateAccountNumbers() {
-		String[] accountNumbers= new String[]{"werwe", "fsdfsf"};
+	private String[] accountNumbers;
+	
+	public String[] getAccountNumbers() {
 		return accountNumbers;
 	}
 	
