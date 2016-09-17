cd75cafa89b9cc8028ffcab8cc1f2b52631daa6f
rokervich <rokervich@rokervich.com>
2016-09-17 23:16:49 +0200

    add db connection code

--- src/main/java/com/openshift/aeg/AccountNumberGenerator.java
+++ src/main/java/com/openshift/aeg/AccountNumberGenerator.java
@@ -1,8 +1,12 @@
 package com.openshift.aeg;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.stream.IntStream;
 
 public class AccountNumberGenerator implements Serializable{
 	/**
@@ -12,7 +16,6 @@ public class AccountNumberGenerator implements Serializable{
 	private final int MAX_ACCOUNT_NUMBERS = 5;
 
 	{
-		//accountNumbers= new String[]{"AAA-3254234234", "BBB-2434597938"};
 		accountNumbers= generateAccountNumbers();
 	}
 	
@@ -27,19 +30,39 @@ public class AccountNumberGenerator implements Serializable{
 		this.accountNumbers = accountNumbers;
 	}
 	
-	public String[] generateAccountNumbers() {
-		String[] accountPrefixes = {"AAA","BBB","CCC","DDD"};
-		accountNumbers = new String[MAX_ACCOUNT_NUMBERS];
-		for (int i=0; i<MAX_ACCOUNT_NUMBERS; i++) {
-			String accountNumberPrefix = accountPrefixes[new Random().nextInt(accountPrefixes.length)];
-			int accountNumberSuffix = 1000000 + new Random().nextInt(9000000); 
-			accountNumbers[i] = new String(accountNumberPrefix + "-" + accountNumberSuffix);
-		}
-		return accountNumbers;
-	}
+//	public String[] generateAccountNumbers() {
+//		String[] accountPrefixes = {"AAA","BBB","CCC","DDD"};
+//		accountNumbers = new String[MAX_ACCOUNT_NUMBERS];
+//		for (int i=0; i<MAX_ACCOUNT_NUMBERS; i++) {
+//			String accountNumberPrefix = accountPrefixes[new Random().nextInt(accountPrefixes.length)];
+//			int accountNumberSuffix = 1000000 + new Random().nextInt(9000000); 
+//			accountNumbers[i] = new String(accountNumberPrefix + "-" + accountNumberSuffix);
+//		}
+//		return accountNumbers;
+//	}
 	
-	public static void main(String[] args) {
-		AccountNumberGenerator x = new AccountNumberGenerator();
-		x.getAccountNumbers();
+	public String[] generateAccountNumbers() {
+		ArrayList<String> list = new ArrayList<String>();
+		try {
+				String databaseURL = "jdbc:postgresql://";
+				databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+				databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+				String username = System.getenv("POSTGRESQL_USER");
+				String password = System.getenv("PGPASSWORD");
+				Connection connection = DriverManager.getConnection(databaseURL, username, password);
+				if (connection != null) {
+					String SQL = "select a.ac_number as account_number from account_number a";
+					Statement stmt = connection.createStatement();
+					ResultSet rs = stmt.executeQuery(SQL);
+					while (rs.next()) {
+						list.add(rs.getString("account_number"));
+					}
+					rs.close();
+					connection.close();
+				}
+		} catch (Exception e) {
+			System.out.println("Database connection problem!");
+		}
+		return list.toArray(new String[list.size()]);	  
 	}
 }
