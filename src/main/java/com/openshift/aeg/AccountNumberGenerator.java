4305de716e1153943ed6a418983984d1778865e9
rokervich <rokervich@rokervich.com>
2016-09-17 15:15:29 +0200

    modify AccountNumberGenerator

--- src/main/java/com/openshift/aeg/AccountNumberGenerator.java
+++ src/main/java/com/openshift/aeg/AccountNumberGenerator.java
@@ -1,18 +1,22 @@
 package com.openshift.aeg;
 
 import java.io.Serializable;
+import java.util.Random;
 
 public class AccountNumberGenerator implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private final int MAX_ACCOUNT_NUMBERS = 5;
 
 	{
-		accountNumbers= new String[]{"AAA-3254234234", "BBB-2434597938"};
+		//accountNumbers= new String[]{"AAA-3254234234", "BBB-2434597938"};
+		accountNumbers= generateAccountNumbers();
 	}
 	
 	private String[] accountNumbers;
+	private String[] accountPrefixes = {"AAA","BBB","CCC","DDD"};
 	
 	public String[] getAccountNumbers() {
 		return accountNumbers;
@@ -22,4 +26,11 @@ public class AccountNumberGenerator implements Serializable{
 		this.accountNumbers = accountNumbers;
 	}
 	
+	public String[] generateAccountNumbers() {
+		String[] newAccountNumbers = new String[MAX_ACCOUNT_NUMBERS];
+		for (int i=0; i<MAX_ACCOUNT_NUMBERS; i++) {
+			newAccountNumbers[i] = accountPrefixes[new Random().nextInt(accountPrefixes.length)];
+		}
+		return newAccountNumbers;
+	}
 }
